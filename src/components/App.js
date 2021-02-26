baa11f8544b0f0e7ad36c07def358aaea351cab9
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-25 21:20:51 -0500

    add function in app to add new items to the todo list

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [newInput, setNewInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(e) {
+    const input = e.target.value;
+    console.log(input);
+    setNewInput(input);
+  }
+
+  function handleClick() {
+    setItems((prev) => {
+      return [...prev, newInput];
+    });
+    console.log(items);
+    setNewInput("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={newInput} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((item) => {
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
