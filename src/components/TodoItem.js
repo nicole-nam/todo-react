f614c717b4eac4d2f31ac14caa30fea6e402bce5
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-25 22:01:47 -0500

    add line through style when item clicked

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoItem(props) {
-  return <li>{props.text}</li>;
+  const [click, setClick] = useState(false);
+
+  function handleClick() {
+    console.log(click);
+    setClick(true);
+  }
+
+  return (
+    <div onClick={handleClick}>
+      <li
+        style={
+          click
+            ? { textDecoration: "line-through" }
+            : { textDecoration: "none" }
+        }
+      >
+        {props.text}
+      </li>
+    </div>
+  );
 }
 
 export default TodoItem;
